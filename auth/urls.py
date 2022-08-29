acf8d757ed9e99026e8eaa92e40fa3e3c56fc7fe
sorena <sorenapaydar81@gmail.com>
2022-08-30 03:38:02 +0430

    wip

--- auth/urls.py
+++ auth/urls.py
@@ -6,5 +6,5 @@ from .views import RegisterView
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("register/", RegisterView.as_view(), name="register_view"),
+    path("register/", RegisterView.as_view(), name="auth_register"),
 ]
