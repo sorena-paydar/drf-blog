eb6448695b6e20632af2c8eec71c6b6680e3a6d0
sorena <sorenapaydar81@gmail.com>
2022-08-30 15:44:03 +0430

    fix drf login button

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("auth.urls")),
     path("blogs/", include("blogs.urls")),
+    path("", include("rest_framework.urls")),
 ]
